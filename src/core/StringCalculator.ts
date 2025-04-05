feb726378a63029c82c95f76bb0946f64444e5c7
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-05 21:54:41 +0530

    refactor: apply clean code principles to StringCalculator

--- src/core/StringCalculator.ts
+++ src/core/StringCalculator.ts
@@ -1,26 +1,42 @@
 export class StringCalculator {
   private delimiterPrefix: string = "//";
-  private delimiterSufix: string = "\n";
+  private delimiterSuffix: string = "\n";
   private defaultDelimiterRegex: RegExp = new RegExp(/,|\n/);
 
-  add(numbers: string): number {
-    if (!numbers) return 0;
+  add(numberString: string): number {
+    if (!numberString) return 0;
 
-    let delimiterRegex = this.defaultDelimiterRegex;
-
-    if (numbers.startsWith(this.delimiterPrefix)) {
-      const delimiterEndIndex = numbers.indexOf(this.delimiterSufix);
-      const customDelimiter = numbers.substring(
-        this.delimiterPrefix.length,
-        delimiterEndIndex
-      );
-      delimiterRegex = new RegExp(customDelimiter);
-      numbers = numbers.slice(delimiterEndIndex + 1);
-    }
-
-    const nums = numbers.split(delimiterRegex).map(Number);
-    const sum = nums.reduce((acc, num) => acc + num, 0);
+    const numbers = this.extractNumbers(numberString);
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
 
     return sum;
+  }
+
+  private hasCustomDelimiter(numberString: string): boolean {
+    return numberString.startsWith(this.delimiterPrefix);
+  }
+
+  private getCustomDelimiter(numberString: string): string {
+    const delimiterEndIndex = numberString.indexOf(this.delimiterSuffix);
+
+    return numberString.substring(
+      this.delimiterPrefix.length,
+      delimiterEndIndex
+    );
+  }
+
+  private extractNumbers(numberString: string): number[] {
+    let delimiterRegex = this.defaultDelimiterRegex;
+
+    if (this.hasCustomDelimiter(numberString)) {
+      const customDelimiter = this.getCustomDelimiter(numberString);
+      delimiterRegex = new RegExp(customDelimiter);
+
+      numberString = numberString.slice(
+        numberString.indexOf(this.delimiterSuffix) + 1
+      );
+    }
+
+    return numberString.split(delimiterRegex).map(Number);
   }
 }
