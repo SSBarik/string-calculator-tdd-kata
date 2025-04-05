3790cd1eaca41d5de87823232ec78e77a149c872
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-05 20:52:10 +0530

    feat: handle custom delimiters

--- src/core/StringCalculator.ts
+++ src/core/StringCalculator.ts
@@ -2,7 +2,16 @@ export class StringCalculator {
   add(numbers: string): number {
     if (!numbers) return 0;
 
-    const nums = numbers.split(/,|\n/).map(Number);
+    let delimiterRegex = new RegExp(/,|\n/);
+
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf("\n");
+      const customDelimiter = numbers.substring(2, delimiterEndIndex);
+      delimiterRegex = new RegExp(customDelimiter);
+      numbers = numbers.slice(delimiterEndIndex + 1);
+    }
+
+    const nums = numbers.split(delimiterRegex).map(Number);
     const sum = nums.reduce((acc, num) => acc + num, 0);
 
     return sum;
