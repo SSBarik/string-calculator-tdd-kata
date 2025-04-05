cf02bbac1aac457ae0d0af4f0381e65623adff27
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-05 20:26:49 +0530

    test: add test to handle custom delimiter

--- src/__tests__/core/StringCalculator.test.ts
+++ src/__tests__/core/StringCalculator.test.ts
@@ -19,3 +19,7 @@ test("returns sum of multiple comma-separated numbers", () => {
 test("handles new lines between numbers", () => {
   expect(new StringCalculator().add("1\n2,3")).toBe(6);
 });
+
+test("handles custom delimiter using // syntax", () => {
+  expect(new StringCalculator().add("//;\n1;2")).toBe(3);
+});
