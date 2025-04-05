21bf914af08bf5a0c49b3754867c1efed6fbc1e1
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-05 20:21:49 +0530

    feat: extend add() to handle both comma and new line as delimiters

--- src/core/StringCalculator.ts
+++ src/core/StringCalculator.ts
@@ -2,7 +2,7 @@ export class StringCalculator {
   add(numbers: string): number {
     if (!numbers) return 0;
 
-    const nums = numbers.split(",").map(Number);
+    const nums = numbers.split(/,|\n/).map(Number);
     const sum = nums.reduce((acc, num) => acc + num, 0);
 
     return sum;
