6abba5b6260346a1a512bfe93686ce78993d1b26
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-05 19:06:42 +0530

    test: add test for single number input

--- tests/core/StringCalculator.test.ts
+++ tests/core/StringCalculator.test.ts
@@ -1,5 +1,9 @@
 import { StringCalculator } from "core/StringCalculator";
 
 test("returns 0 for empty string", () => {
-  expect(new StringCalculator().add("").toBe(0));
+  expect(new StringCalculator().add("")).toBe(0);
+});
+
+test("returns the number for string with single number", () => {
+  expect(new StringCalculator().add("1")).toBe(1);
 });
