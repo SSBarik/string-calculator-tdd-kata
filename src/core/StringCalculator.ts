26bb5418774e2d1efd031a7c5e19f2db6c5373a9
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-05 21:04:01 +0530

    refactor: extract delimiter constants in StringCalculator

--- src/core/StringCalculator.ts
+++ src/core/StringCalculator.ts
@@ -1,12 +1,19 @@
 export class StringCalculator {
+  private delimiterPrefix: string = "//";
+  private delimiterSufix: string = "\n";
+  private defaultDelimiterRegex: RegExp = new RegExp(/,|\n/);
+
   add(numbers: string): number {
     if (!numbers) return 0;
 
-    let delimiterRegex = new RegExp(/,|\n/);
+    let delimiterRegex = this.defaultDelimiterRegex;
 
-    if (numbers.startsWith("//")) {
-      const delimiterEndIndex = numbers.indexOf("\n");
-      const customDelimiter = numbers.substring(2, delimiterEndIndex);
+    if (numbers.startsWith(this.delimiterPrefix)) {
+      const delimiterEndIndex = numbers.indexOf(this.delimiterSufix);
+      const customDelimiter = numbers.substring(
+        this.delimiterPrefix.length,
+        delimiterEndIndex
+      );
       delimiterRegex = new RegExp(customDelimiter);
       numbers = numbers.slice(delimiterEndIndex + 1);
     }
